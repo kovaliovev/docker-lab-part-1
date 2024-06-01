25798635f7aaaae296ca5a2d7c85fddbbbd54b1c
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-06-01 16:42:37 +0300

    Add a dummy comment

--- spaceship/app.py
+++ spaceship/app.py
@@ -5,6 +5,7 @@ from starlette.responses import FileResponse
 from spaceship.config import Settings
 from spaceship.routers import api, health
 
+# just a dummy comment
 
 def make_app(settings: Settings) -> FastAPI:
     app = FastAPI(
